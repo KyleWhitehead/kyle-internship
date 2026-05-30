e3f36736e948218e09b8c2c702114992e177be37
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-05-30 11:44:41 -0600

    Installed React Slick Carousel

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
 import axios from "axios";
+import Slider from "react-slick";
 
 const HotCollections = () => {
     const [collections, setCollections] = useState([]);
@@ -22,6 +23,8 @@ const HotCollections = () => {
     fetchCollections();
   }, []);
 
+  
+
   return (
     <section id="section-collections" className="no-bottom">
       <div className="container">
@@ -32,29 +35,29 @@ const HotCollections = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          {collections.map((collection, index) => (
-            <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
-              <div className="nft_coll">
-                <div className="nft_wrap">
-                  <Link to={`/item-details/${collection.nftId}`}>
-                    <img src={collection.nftImage} className="lazy img-fluid" alt={collection.title} />
-                  </Link>
-                </div>
-                <div className="nft_coll_pp">
-                  <Link to="/author">
-                    <img className="lazy pp-coll" src={collection.authorImage} alt={collection.authorName} />
-                  </Link>
-                  <i className="fa fa-check"></i>
-                </div>
-                <div className="nft_coll_info">
-                  <Link to="/explore">
-                    <h4>{collection.title}</h4>
-                  </Link>
-                  <span>{collection.code}</span>
+            {collections.map((collection, index) => (
+              <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12" key={index}>
+                <div className="nft_coll">
+                  <div className="nft_wrap">
+                    <Link to={`/item-details/${collection.nftId}`}>
+                      <img src={collection.nftImage} className="lazy img-fluid" alt={collection.title} />
+                    </Link>
+                  </div>
+                  <div className="nft_coll_pp">
+                    <Link to="/author">
+                      <img className="lazy pp-coll" src={collection.authorImage} alt={collection.authorName} />
+                    </Link>
+                    <i className="fa fa-check"></i>
+                  </div>
+                  <div className="nft_coll_info">
+                    <Link to="/explore">
+                      <h4>{collection.title}</h4>
+                    </Link>
+                    <span>{collection.code}</span>
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            ))}
         </div>
       </div>
     </section>
